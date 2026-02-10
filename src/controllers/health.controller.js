3be3d07c046c8093c45b21ea79665c301787190b
maridulwalia <maridulwalia@gmail.com>
2026-02-10 12:52:15 +0530

    New work

--- src/controllers/health.controller.js
+++ src/controllers/health.controller.js
@@ -4,6 +4,9 @@ const handleHealth = (req, res) => {
     return res.status(HTTP_STATUS.OK).json({
         status: 'OK',
         timestamp: new Date().toISOString(),
+        official_mailId: 'maridul1897.be23@chitkara.edu.in',
+        message: "Success"
+        
     });
 };
 
